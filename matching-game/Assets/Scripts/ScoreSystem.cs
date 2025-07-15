e5f942a90365185c21b05ecd3519706962b4f9cd
KeirenWhite <143643209+KeirenWhite@users.noreply.github.com>
2025-07-15 14:22:45 -0400

    score system finished, main template now done!

--- matching-game/Assets/Scripts/ScoreSystem.cs
+++ matching-game/Assets/Scripts/ScoreSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +11,22 @@ public class ScoreSystem : MonoBehaviour
     public float scoreMult = 1.0f;
     public float baseScore = 10f;
     public float currentScoreMult;
+    public TMP_Text scoreText;
+    public TMP_Text streakText;
+    public TMP_Text endScoreText;
 
-    private void Update()
+    private void FixedUpdate()
     {
         Debug.Log(score);
         Debug.Log(matchCounter);
+        UpdateCounterDisplay();
+    }
+
+    public void UpdateCounterDisplay()
+    {
+        scoreText.text = string.Format("Score: {0}", score);
+        streakText.text = string.Format("Streak: {0}", matchCounter);
+        endScoreText.text = string.Format("{0}", score);
     }
 
     public void ScoreMultiplier()
